f74688c7ae43d029b25e200abbd0167191feaf0c
The-uncanny-valley <runcannyvalley@gmail.com>
2026-02-23 19:49:09 +0300

    feat(ui): improve image loading with SubcomposeAsyncImage
    Replaced custom gif with CircularProgressIndicator.

--- composeApp/src/androidMain/kotlin/com/uncannyvalley/myproject/ImageLoader.kt
+++ composeApp/src/androidMain/kotlin/com/uncannyvalley/myproject/ImageLoader.kt
@@ -1,44 +1,44 @@
 package com.uncannyvalley.myproject
 
-import androidx.compose.foundation.Image
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import myproject.composeapp.generated.resources.Res
-import myproject.composeapp.generated.resources.loading
-import myproject.composeapp.generated.resources.placeholder
-import org.jetbrains.compose.resources.painterResource
+import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 
 actual class ImageLoader actual constructor() {
+
     @Composable
     actual fun LoadImage(
         url: String,
         contentDescription: String,
         modifier: Modifier
     ) {
-        val painter = rememberAsyncImagePainter(
+        SubcomposeAsyncImage(
             model = url,
-            placeholder = painterResource(Res.drawable.loading),
-            error = painterResource(Res.drawable.placeholder)
-        )
-
-        // tint for loading or error state
-        val colorFilter = when (painter.state) {
-            is AsyncImagePainter.State.Loading,
-            is AsyncImagePainter.State.Error -> ColorFilter.tint(MaterialTheme.colorScheme.primary)
-            else -> null
-        }
-
-        Image(
-            painter = painter,
             contentDescription = contentDescription,
             modifier = modifier,
             contentScale = ContentScale.Fit,
-            colorFilter = colorFilter
+
+            loading = {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            },
+
+            error = {
+                AsyncImage(
+                    model = R.drawable.placeholder,
+                    contentDescription = null
+                )
+            }
         )
     }
 }
