e404598da153919ee4a81c1aca1078bff2df9a42
The-uncanny-valley <runcannyvalley@gmail.com>
2026-02-23 16:00:45 +0300

    feat(ui): support placeholder and error images in ImageLoader

--- composeApp/src/androidMain/kotlin/com/uncannyvalley/myproject/ImageLoader.kt
+++ composeApp/src/androidMain/kotlin/com/uncannyvalley/myproject/ImageLoader.kt
@@ -1,9 +1,14 @@
 package com.uncannyvalley.myproject
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import myproject.composeapp.generated.resources.Res
+import myproject.composeapp.generated.resources.loading
+import myproject.composeapp.generated.resources.placeholder
+import org.jetbrains.compose.resources.painterResource
 
 actual class ImageLoader actual constructor() {
     @Composable
@@ -12,8 +17,14 @@ actual class ImageLoader actual constructor() {
         contentDescription: String,
         modifier: Modifier
     ) {
-        AsyncImage(
+        val painter = rememberAsyncImagePainter(
             model = url,
+            placeholder = painterResource(Res.drawable.loading),
+            error = painterResource(Res.drawable.placeholder)
+        )
+
+        Image(
+            painter = painter,
             contentDescription = contentDescription,
             modifier = modifier,
             contentScale = ContentScale.Fit
